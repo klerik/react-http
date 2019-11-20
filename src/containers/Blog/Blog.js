1cb0efd71c7d07d39b006733dc20c05f4744f0ee
kleri <NTjwRq6DNTjwRq6D>
2019-11-20 08:28:31 +0200

    Initial commit

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import {Route, NavLink} from 'react-router-dom';
+import Posts from './Posts/Posts';
+
+import './Blog.css';
+import NewPost from './NewPost/NewPost';
+
+
+class Blog extends Component {
+    render () {
+        return (
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink
+                                to="/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}
+                            >Home</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New post</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/*<Route path="/" exact render={() => <h1>Home</h1>}/>*/}
+                <Route path="/" exact component={Posts}/>
+                <Route path="/new-post" exact component={NewPost}/>
+            </div>
+        );
+    }
+}
+
+export default Blog;
